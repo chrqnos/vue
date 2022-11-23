1dc1a5a5f7468e73201651793dd303ff356d8e29
chrqnos <huzfisyed@gmail.com>
2022-11-22 22:43:11 -0500

    Added Files

---
+++ vite-project/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Movie from "./components/Movie.vue";
+</script>
+
+<template>
+  <div class="app-container">
+    <Suspense>
+      <Movie />
+    </Suspense>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ vite-project/src/components/Movie.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from "vue";
+import axios from "axios";
+
+const id = ref("");
+const movie = ref(null);
+
+const getMovies = async () => {
+  const data = id.value;
+  movie.value = (
+    await axios.get("https://api.themoviedb.org/3/search/movie", {
+      params: {
+        api_key: "0a4dbc5e3b74420548bdd09d59591acf",
+        query: data,
+      },
+    })
+  ).data;
+};
+</script>
+
+<template>
+  <select v-model="id">
+    <option value="293660">Deadpool</option>
+  </select>
+  <button @click="getMovies">Get</button>
+  <div v-if="movie" class="movie-container">
+    <p>Overview: {{movie.overview}}</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
