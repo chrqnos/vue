35bcf4121257d48db17b6aafbf2a1be04325b631
chrqnos <huzfisyed@gmail.com>
2022-11-23 23:37:11 -0500

    Made it work.

--- vite-project/src/components/Movie.vue
+++ vite-project/src/components/Movie.vue
@@ -20,14 +20,89 @@ const getMovies = async () => {
 
 <template>
   <select v-model="id">
-    <option value="293660">Deadpool</option>
+    <option value="Deadpool">Deadpool</option> <!-- 293660 -->
+    <option value="Mugen Train">Demon Slayer</option>
+    <option value="Jujutsu Kaisen">Jujutsu Kaisen</option>    
+    <option value="Transformers">Transformers</option>
+    <option value="A Silent Voice">A Silent Voice</option>
+    <option value="Midnight Runners">Midnight Runners</option> 
+    <option value="Fight Club">Fight Club</option>   
+    <option value="Blade Runner">Blade Runner</option>  
+    <option value="American Psycho">American Psycho</option>   
+    <option value="Pacific Rim">Pacific Rim</option>  
   </select>
   <button @click="getMovies">Get</button>
   <div v-if="movie" class="movie-container">
-    <p>Overview: {{movie.overview}}</p>
+    <h1>{{movie.results[0].original_title}}</h1>
+    <p>Overview: {{movie.results[0].overview}}</p>
+    <!-- <img src = https://image.tmdb.org/t/p/w500 {{movie.results[0].poster_path}}> -->
+    <p>Release Date: {{movie.results[0].release_date}} | Movie Popularity: {{movie.results[0].popularity}}</p>
+    <p>Adult: {{movie.results[0].adult}} | Language: {{movie.results[0].original_language}}</p>
+    <p>Vote Average: {{movie.results[0].vote_average}} | Vote Count: {{movie.results[0].vote_count}}</p>
+
   </div>
 </template>
 
 <style scoped>
+*{
+  margin: 0;
+  padding: 5px;
+  font-family:'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif
+}
+body {
+  color: blueviolet;
+}
 
+select{
+  color: white;
+  border-radius:50px;
+  text-align: center;
+  margin-left:45%;
+  border: 5px solid black;
+  background-color: rgb(159, 92, 221);
+}
+
+button{
+  color: white;
+  border-radius:50px;
+  text-align: center;
+  border: 5px solid black;
+  background-color: rgb(159, 92, 221);
+}
+
+p{
+  color: white;
+  text-align: center;
+  border: 5px solid black;
+  background-color: rgb(159, 92, 221);
+  border-radius: 50px;
+}
+
+img{
+  border-radius: 50px;
+  margin-left:15%;
+  aspect-ratio: 2 / 3;
+  border: 10px solid black;
+  height: 700px;
+  background-color: rgb(159, 92, 221);
+}
+
+iframe{
+  border-radius:50px;
+  margin-bottom: 10%;
+  margin-left: 15%;
+  aspect-ratio: 16 / 9;
+  height: 400px;
+  border: 10px solid black;
+  background-color: rgb(159, 92, 221);
+}
+
+h1{
+  text-align: center;
+  font-size: 100px;
+  color: white;
+  border: 5px solid black;
+  background-color: rgb(159, 92, 221);
+  border-radius: 50px;
+}
 </style>
